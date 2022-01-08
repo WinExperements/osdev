43e641a8ad885983ce8887e4f98aeea97b23165c
Sergij Yevchuk <85248187+WinExperements@users.noreply.github.com>
2022-01-09 00:46:25 +0200

    Create typedefs.h

---
+++ kernel/inc/typedefs.h
@@ -0,0 +1,9 @@
+#ifndef TYPEDEFS_H
+#define TYPEDEFS_H
+typedef unsigned int   u32;
+typedef          int   s32;
+typedef unsigned short u16;
+typedef          short s16;
+typedef unsigned char  u8;
+typedef          char  s8;
+#endif
