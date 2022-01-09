7cce054fb898ff8f10b1a140dfe7dd802969feff
Sergij Yevchuk <85248187+WinExperements@users.noreply.github.com>
2022-01-09 19:13:34 +0200

    Update mm.h

--- kernel/inc/mm.h
+++ kernel/inc/mm.h
@@ -1,4 +1,14 @@
 #ifndef MM_H
 #define MM_H
+#include "typedefs.h"
+typedef struct chunk_t {
+	struct chunk_t *next;
+	struct chunk_t *prev;
+	int alloc;
+	u32 size;
+} chunk_t;
 void *memset(void *,char,int);
+void mm_init(u32,u32);
+void *kmalloc(u32);
+void kfree(void *);
 #endif
