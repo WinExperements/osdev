a57733735fd47be396bae2822a89970a2bb882f1
Sergij Yevchuk <85248187+WinExperements@users.noreply.github.com>
2022-01-09 19:16:42 +0200

    Update printk.c
    Added some useful functions

--- kernel/src/x86/printk.c
+++ kernel/src/x86/printk.c
@@ -134,3 +134,15 @@ void printkDec(int n) {
     }
     printk(c2);
 }
+void printkPos(int x,int y,char *msg) {
+	cursor_x = x;
+	cursor_y = y;
+	printk(msg);
+}
+void printkHex(u8 key) {
+	char* foo = "00";
+    char* hex = "0123456789ABCDEF";
+    foo[0] = hex[(key >> 4) & 0xF];
+    foo[1] = hex[key & 0xF];
+    printk(foo);
+}
