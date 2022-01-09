b55582b6b07a8d197a04e4feb93f08b5eca1a566
Sergij Yevchuk <85248187+WinExperements@users.noreply.github.com>
2022-01-09 19:25:52 +0200

    Update arch.c
    Added timer initialisation, and rewritten some code

--- kernel/src/x86/arch.c
+++ kernel/src/x86/arch.c
@@ -2,10 +2,16 @@
 #include "printk.h"
 #include "arch/x86/gdt.h"
 #include "interrupt.h"
+#include "timer.h"
 void arch_setup() {
 	printk_init();
 	printk_clear();
+	printk("WARRNING THIS BUILD IS A DEBUG NOT RELEASE!\n");
+	printk("Initializing GDT...");
 	gdt_init();
-    intr_init();
-	printk("arch: set-up complete\n");
+	printk("ok\nInitializing interrupts...");
+        intr_init();
+	printk("ok\nInitializing timer...");
+	timer_init(100); 
+	printk("ok\nset-up complete\n");
 }
