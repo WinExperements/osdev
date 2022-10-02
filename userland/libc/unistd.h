10dee96a7ed5aae8ec1448cedd6da8df7bf125dd
Sergij Yevchuk <85248187+WinExperements@users.noreply.github.com>
2022-10-02 17:41:14 +0300

    Very major update part 2
    Yeah, this is our userspace programs and shell, in plans add the GUI

---
+++ userland/libc/unistd.h
@@ -0,0 +1,8 @@
+#ifndef UNISTD_H
+#define UNISTD_H
+#include "wait.h"
+char *pwd(char *path,int len);
+void chdir(char *path);
+int execv(char *file,int argc,char *argv[]);
+pid_t getppid();
+#endif
