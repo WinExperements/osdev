efe0bfd22dcd6b076aaaee000d97b1996e788bf1
WinExperements <sergijyevchuk@gmail.com>
2022-10-26 22:20:07 +0300

    synced code with the latest version

--- src/module/atapi/atapi.c
+++ src/module/atapi/atapi.c
@@ -5,4 +5,40 @@
 #include<dev.h>
 #include<mm/pmm.h>
 #include<interrupts.h>
-/* This driver currently doesn't do nothing */
+// === Internal functions here ===
+typedef struct {
+	uint16_t base;
+	uint16_t ctrl;
+	uint16_t nein;
+} ata_regs_t;
+typedef struct {
+	uint8_t reserved;
+	uint8_t channel;
+	uint8_t drive;
+	uint8_t type;
+	uint16_t signature;
+	uint16_t sup;
+	uint32_t cmd_sets;
+	uint32_t size;
+	char model[41];
+	ata_regs_t *regs;
+} ata_device_t;
+void ata_io_wait(ata_device_t *dev) {
+	io_readPort(dev->regs->base+0x0c);
+	io_readPort(dev->regs->base+0x0c);
+	io_readPort(dev->regs->base+0x0c);
+	io_readPort(dev->regs->base+0x0c);
+}
+int ata_status_wait(ata_device_t *dev,int timeout) {
+	int status;
+	if (timeout > 0) {
+		int i =0;
+		while((status = io_readPort(dev->regs->base+0x07) & 0x0 && (i < timeout)) i++;
+	} else {
+		while((status = io_readPort(dev->regs->base+0x07)) & 0x0);
+	}
+	return status;	
+}
+// === Public functions here ===
+void atapi_init() {
+}
